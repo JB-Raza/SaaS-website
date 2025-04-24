7fdc49309d73de826831085584cbe7fc1fe911d5
Jahanzaib Raza <jraza198@gmail.com>
2025-04-24 18:20:46 +0500

    added app integration and pricing section

--- src/App.jsx
+++ src/App.jsx
@@ -19,18 +19,21 @@ import TaskManagement from './components/sections/TaskManagement.jsx'
 import Workspace from './components/sections/Workspace.jsx'
 import AutomateTasks from './components/sections/AutomateTasks.jsx'
 import TrackProjects from "./components/sections/TrackProjects.jsx";
+import Testimonial from "./components/sections/Testimonial.jsx";
+import Button from "./components/universalComponents/Button.jsx";
+import Pricing from "./components/sections/Pricing.jsx";
 
 function App() {
 
   return (
     <>
-      <div className="hero-section relative">
+      {/* <div className="hero-section relative">
         <Navbar />
         <Hero />
-      </div>
+      </div> */}
 
       {/* our clients saas companies */}
-      <div className="clients-section flex gap-10 justify-center py-10 my-15">
+      {/* <div className="clients-section flex gap-10 justify-center py-10 my-15">
         <div className="container relative">
           <h5 className="text-center mb-16 font-semibold line-clamp-1 heading-4">Loved by next-gen B2B <span className='text-gradient-teal'>SaaS</span> companies.....</h5>
 
@@ -81,19 +84,44 @@ function App() {
             </SwiperSlide>
           </Swiper>
         </div>
-      </div>
+      </div> */}
 
       {/* task management features */}
-      <TaskManagement />
+      {/* <TaskManagement /> */}
 
       {/* workspace departments */}
-      <Workspace />
+      {/* <Workspace /> */}
 
       {/* built-in automation */}
-      <AutomateTasks />
+      {/* <AutomateTasks /> */}
 
       {/* track projects */}
-        <TrackProjects />
+      {/* <TrackProjects /> */}
+
+      {/* testimonial section */}
+      {/* <Testimonial /> */}
+
+      {/* app integration section */}
+
+      {/* <div className="app-integration relative justify-between h-[500px] max-h-[700px]">
+        <img src="./app-integration/app-integration-center.png" alt="error"
+          className="absolute top-1/2 left-1/2 w-15 h-15 md:w-20 md:h-20 -translate-x-1/2 -translate-y-1/2"
+        />
+        <h2 className="heading-2 text-center font-semibold mt-4 leading-10  capitalize">Avoid <span className="italic font-normal text-gradient-teal">distractions</span> with app integrations</h2>
+
+        <div className="relative">
+          <img src="./app-integration/1.png" alt="" className="absolute left-3 md:left-1/9 w-10 md:w-15 "/>
+          <img src="./app-integration/2.png" alt="" className="absolute translate-y-30 sm:translate-y-35 md:translate-y-40 left-1/8 md:left-1/5 w-10 md:w-15"/>
+          <img src="./app-integration/3.png" alt="" className="absolute translate-y-60 md:translate-y-70 left-1 md:left-1/9 w-15 h-15 "/>
+          <img src="./app-integration/4.png" alt="" className="absolute right-3 md:right-1/9 w-10 md:w-15 "/>
+          <img src="./app-integration/5.png" alt="" className="absolute translate-y-30 sm:translate-y-35 md:translate-y-40 right-1/8 md:right-1/5 w-10 md:w-15"/>
+          <img src="./app-integration/6.png" alt="" className="absolute translate-y-60 md:translate-y-70 right-1 md:right-1/9 w-15 h-15 "/>
+        </div>
+        <Button content={"start free trial"} className={"absolute bottom-0 left-1/2 -translate-x-1/2 text-white px-12 !py-5"} icon={"fa-solid fa-cloud-arrow-down"} />
+      </div> */}
+
+      {/* pricing section */}
+      <Pricing />
 
     </>
   )
---
+++ src/components/sections/Testimonial.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+
+// swiper css
+import 'swiper/css';
+
+
+export default function Testimonial() {
+    return (
+        <div className="grid grid-cols-12 bg-[#06766E] py-15 px-3 sm:px-6 mx-auto w-[calc(100%-10px)] sm:w-[calc(100%-30px)] rounded-xl">
+            {/* col 1 */}
+            <div className="col-span-12 md:col-span-5 lg:col-span-4">
+                <img
+                    className="mx-auto"
+                    src="./testimonial/testimonials-three-thumbs.png" alt="" />
+            </div>
+            {/* col 2 swiper */}
+            <Swiper className="col-span-12 md:col-span-7 lg:col-span-8 mt-10 md:mt-0 overflow-clip w-full"
+                slidesPerView={1}
+                grabCursor={true}
+            >
+                {/* slide 1 */}
+                <SwiperSlide className="!flex flex-col gap-7 items-start px-2 sm:px-4 md:pe-15">
+                    <img src="./testimonial/ratings.svg" alt="" />
+                    <p className="heading-4 line-clamp-3 font-normal text-neutral-100 text-wrap">Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto aspernatur illo animi laboriosam dolorum veritatis officia quis dolorem quos distinctio.</p>
+                    <hr className="border-t-[1px] border-white/20 mt-5 w-full" />
+                    <div className="card flex gap-7 items-center bg-white/10 px-7 py-2">
+                        <img src="./testimonial/client-img.png" alt="client" />
+                        <div>
+                            <h5 className="text-[20px] font-semibold text-white">Tom Hiddleson</h5>
+                            <p className="font-semibold text-white"> <span className="text-green-400">CEO</span> and Founder</p>
+                        </div>
+                    </div>
+                </SwiperSlide>
+                {/* slide 2 */}
+                <SwiperSlide className="!flex flex-col gap-7 items-start px-2 sm:px-4 md:pe-15">
+                    <img src="./testimonial/ratings.svg" alt="" />
+                    <p className="heading-4 line-clamp-3 font-normal text-neutral-100 text-wrap">Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto aspernatur illo animi laboriosam dolorum veritatis officia quis dolorem quos distinctio.</p>
+                    <hr className="border-t-[1px] border-white/20 mt-5 w-full" />
+                    <div className="card flex gap-7 items-center bg-white/10 px-7 py-2">
+                        <img src="./testimonial/client-img.png" alt="client" />
+                        <div>
+                            <h5 className="text-[20px] font-semibold text-white">Tom Hiddleson</h5>
+                            <p className="font-semibold text-white"> <span className="text-green-400">CEO</span> and Founder</p>
+                        </div>
+                    </div>
+                </SwiperSlide>
+            </Swiper>
+        </div>
+    )
+}
