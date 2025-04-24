56a9472e11c06ad551f878e0b7b424ece4a89770
Jahanzaib Raza <jraza198@gmail.com>
2025-04-24 12:43:40 +0500

    automation section added

---
+++ src/components/sections/AutomateTasks.jsx
@@ -0,0 +1,87 @@
+import React from 'react'
+import Button from "../universalComponents/Button.jsx";
+
+
+export default function AutomateTasks() {
+    return (
+        <div>
+            {/* task section 1/2 */}
+            <div className="py-15 w-full px-3">
+                <p className="font-medium text-wrap text-center capitalize !bg-white w-fit px-10 py-2 rounded-full border-1 border-[hsl(144,80%,55%)]">
+                    <span className="text-gradient-teal">Up to </span>
+                    <span className="text-yellow-500">70%</span>
+                    <span className="text-gradient-teal"> off managed cloud hosting</span>
+                </p>
+                <h2 className="heading-2 font-semibold mt-4 leading-10 capitalize lg:w-[50%] xxl:w-auto">Supercharge  <span className="italic text-gradient-teal font-normal">daily work</span>. with built-in automation</h2>
+
+                <div className="grid grid-cols-12">
+                    {/* col 1 */}
+                    <div className="col-span-12 lg:col-span-6">
+                        <p className="text-neutral-500 font-medium text-[18px] mt-8 leading-loose">Sassly-CRM in the past allowing you to focus more on your business <br /> simply enjoy your newfound legal time to reflect leaving</p>
+                        {/* features */}
+                        <div className="features my-10 flex flex-col gap-4">
+                            <p className="bg-neutral-100 font-semibold flex gap-3 items-center text-[17px] rounded-full ps-6 pe-15 py-3 w-fit">
+                                <i className="fa-solid fa-square-check text-[20px] text-[#008C63]"></i>
+                                <span>Workflow streamlining</span>
+                            </p>
+                            <p className="bg-neutral-100 font-semibold flex gap-3 items-center text-[17px] rounded-full ps-6 pe-15 py-3 w-fit">
+                                <i className="fa-solid fa-square-check text-[20px] text-[#008C63]"></i>
+                                <span>Expedite onboarding with clear prioritization</span>
+                            </p>
+                            <p className="bg-neutral-100 font-semibold flex gap-3 items-center text-[17px] rounded-full ps-6 pe-15 py-3 w-fit">
+                                <i className="fa-solid fa-square-check text-[20px] text-[#008C63]"></i>
+                                <span>Collaborate on ideas 7x faster</span>
+                            </p>
+                        </div>
+                        <Button content={"Start Free Trial"} icon={"fa-solid fa-cloud-arrow-down"}
+                            className={"!rounded-0 px-13 !py-5"} />
+
+                    </div>
+                    <div className="col-span-12 mt-10 lg:mt-0 lg:col-span-6">
+                        <img src="./automation-thumb.png" alt="" className="mx-auto" />
+                    </div>
+                </div>
+            </div>
+
+            {/* task section 2/2 */}
+            <div className="py-15 w-full px-3">
+                <div className="grid grid-cols-12">
+                    {/* col 1 */}
+                    <div className="col-span-12 mt-10 lg:mt-0 md:col-span-6 pe-7">
+                        <img src="./task-manager-thumb.png" alt="" className="mx-" />
+                    </div>
+                    {/* col 2 */}
+                    <div className="col-span-12 md:col-span-6">
+                        <p className="font-medium text-wrap text-center capitalize !bg-white w-fit px-10 py-2 rounded-full border-1 border-[hsl(144,80%,55%)]">
+                            <span className="text-gradient-teal">Up to </span>
+                            <span className="text-yellow-500">70%</span>
+                            <span className="text-gradient-teal"> off managed cloud hosting</span>
+                        </p>
+                        <h2 className="heading-2 font-semibold mt-4 leading-10 capitalize">Powerful & life-saving <span className="italic text-gradient-teal font-normal"> task manager</span></h2>
+
+                        <p className="text-neutral-500 font-medium text-[18px] mt-8 leading-loose">Sassly-CRM in the past allowing you to focus more on your business <br /> simply enjoy your newfound legal time to reflect leaving</p>
+
+                        {/* card 1 */}
+                        <div className="card flex items-start gap-4 sm:gap-10 mt-13">
+                            <i className="fa-solid fa-bars-progress text-green-500 text-3xl mt-2"></i>
+                            <div>
+                                <h5 className="heading-5 font-semibold">Automate Task Creation</h5>
+                                <p className="text-neutral-600 mt-4 leading-loose wrap-break-word sm:me-10 text-wrap">In today's competitive business, the demand for efficient cost-effective IT solutions has never been more critic.</p>
+                            </div>
+                        </div>
+                        {/* card 2 */}
+                        <div className="card flex items-start gap-4 sm:gap-10 mt-13">
+                            <i className="fa-solid fa-bars-progress text-green-500 text-3xl mt-2"></i>
+                            <div>
+                                <h5 className="heading-5 font-semibold">Automate Task Creation</h5>
+                                <p className="text-neutral-600 mt-4 leading-loose wrap-break-word sm:me-10 text-wrap">In today's competitive business, the demand for efficient cost-effective IT solutions has never been more critic.</p>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/sections/Workspace.jsx
@@ -0,0 +1,64 @@
+import React, {useState} from 'react'
+
+export default function Workspace() {
+      let [activeDepart, setActiveDepart] = useState("all teams")
+    
+    return (
+        <div className="workplaces-section bg-[linear-gradient(69.92deg,_#003D3D_28.63%,_#00664E_87.07%,_#008C63_112.98%)] py-28 w-full">
+
+            <p className="font-medium text-wrap text-center capitalize !bg-white mx-auto w-fit px-10 py-2 rounded-full border-1 border-[hsl(144,80%,55%)]">
+                <span className="text-gradient-teal">Up to </span>
+                <span className="text-yellow-500">70%</span>
+                <span className="text-gradient-teal"> off managed cloud hosting</span>
+            </p>
+
+            <h2 className="heading-2 text-center text-white font-bold mt-4 leading-10">Essential for <span className="italic font-normal">workplaces</span>. Great for <br /> any team.</h2>
+            <p className="text-neutral-300 font-medium text-[18px] text-center mx-auto mt-10 px-3 leading-loose">Sassly-CRM in the past allowing you to focus more on your business <br /> simply enjoy your newfound legal time to reflect leaving</p>
+
+            {/* department container */}
+            <div className="mx-auto p-3 sm:p-7 mt-10 border-[1px] border-white/20 rounded-xl max-w-[1300px]">
+                <div className="department-container rounded-2xl grid grid-cols-12">
+
+                    <div className="col-span-12 lg:col-span-3 flex justify-center items-stretch  px-2 sm:px-7 lg:px-0 ms-0 sm:ms-3 mb-7">
+                        <div className="left-section bg-neutral-100 w-full rounded-2xl mt-24 lg:mt-15">
+                            <div className="logo mx-7 mt-4 mb-20">
+                                <img src="./logo-two.png" alt="" />
+                            </div>
+                            {/* all department btns */}
+                            <div className="flex flex-col">
+                                {/* teams btn */}
+                                <button
+                                    onClick={() => setActiveDepart("all teams")}
+                                    className={`${activeDepart == "all teams" ? "bg-[linear-gradient(270deg,#06766E_0%,#20BA8B_100%)] text-white" : ""} group px-10 py-5 rounded-lg w-[calc(100%-20px)] mx-auto text-[18px] flex items-center justify-start font-semibold gap-4`}>
+                                    <i className="fa-solid fa-shield-halved group-hover:animate-[var(--icon-bubble-animation)]"></i>
+                                    <span>All Teams</span>
+                                </button>
+
+                                {/* security btn */}
+                                <button
+                                    onClick={() => setActiveDepart("security")}
+                                    className={`${activeDepart == "security" ? "bg-[linear-gradient(270deg,#06766E_0%,#20BA8B_100%)] text-white" : ""} group px-10 py-5 rounded-lg w-[calc(100%-20px)] mx-auto text-[18px] flex items-center justify-start font-semibold gap-4`}>
+                                    <i className="fa-solid fa-shield-halved group-hover:animate-[var(--icon-bubble-animation)]"></i>
+                                    <span>Security</span>
+                                </button>
+
+                                {/* technology btn */}
+                                <button
+                                    onClick={() => setActiveDepart("technology")}
+                                    className={`${activeDepart == "technology" ? "bg-[linear-gradient(270deg,#06766E_0%,#20BA8B_100%)] text-white" : ""} group px-10 py-5 rounded-lg w-[calc(100%-20px)] mx-auto text-[18px] flex items-center justify-start font-semibold gap-4`}>
+                                    <i className="fa-solid fa-shield-halved group-hover:animate-[var(--icon-bubble-animation)]"></i>
+                                    <span>Technology</span>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-span-12 lg:col-span-9 my-7 px-7 pt-7">
+                        <div className="">
+                            <img src="./workplace-tab-thumb.png" alt="workspace img" className="mx-auto lg:ms-auto" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
